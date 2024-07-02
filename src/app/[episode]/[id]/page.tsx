b620224b9872e8307b5b61fdb7b9e160c050e974
hrkd <hiroki@hrkd.net>
2024-07-02 12:53:04 +0900

    ⚡️ Update meta data

--- src/app/[episode]/[id]/page.tsx
+++ src/app/[episode]/[id]/page.tsx
@@ -4,6 +4,23 @@ import styles from "./page.module.scss";
 import Link from 'next/link';
 import {TITLE} from '@/constant/'
 import { Disqus } from '@/components/disqus';
+import type { Metadata, ResolvingMetadata } from 'next'
+
+export async function generateMetadata(
+    {params}:{params:{episode:string, id:string}},
+    parent: ResolvingMetadata
+    ): Promise<Metadata> {
+    // read route params
+    const {episode, id} = params;
+    const json = await readFile(episode as keyof Title)
+    const item = json.find((item:any) => item.id === id);
+    const title = (item.text as string).length > 50 ? `${(item.text as string).slice(0,100)}... | starwarsscripts.com`: `${item.text} | starwarsscripts.com`;
+
+    return {
+        title,
+        description: `${item.text} | Star Wars: Episode ${TITLE[episode as keyof Title].number} - ${TITLE[episode as keyof Title].title} | starwarsscripts.com`,
+    }
+} 
 
 export default async function Home({params}:{params:{episode:string, id:string}}) {
     const {episode, id} = params;
